5d0c4916e5f7633fbf86c606da8286227ddf221e
Yasser Benzidiya <ybenzidi@e2r1p10.1337.ma>
2024-12-15 00:19:30 +0100

    yasser

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ybenzidi <ybenzidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 22:24:08 by ybenzidi          #+#    #+#             */
-/*   Updated: 2024/12/13 23:02:29 by ybenzidi         ###   ########.fr       */
+/*   Updated: 2024/12/14 23:58:37 by ybenzidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,25 @@ int	ft_printf(const char *datatype, ...)
 	va_end(args);
 	return (count);
 }
+// variadic fct is fct  can take any amout of arguments 
+// theres 2 types of register gp , fp .
+// gp_ofsset can store int , pointers and flags 
+// fp store flout and doubles
+// theres 16 gp purpose registers and only the first 6 are used for arg
+// same thing for fp purpose but the 8 first registers for args
+// gp registers hold 8 bytes and fp hold 16 bytes : for evrey register  
+
+// regitser storage place in cpu to hold data and instuctions 
+// when , when we have ram : register aare workspace for cpu 
+// register allow the cpu to manipulate and store data during in execution fatser then ram 
+// for storing intermi values like res of calculation 
 
 // strdarg declar 4 macros
 // macro is a code segment that is replaced during compilation
-//va_list data type
+// va_list data type to handle variable arg 
+// gp ofsset
+// fp ofsset 
+// overflowarea
+//
+// va list where the arguments will be stored
 //
